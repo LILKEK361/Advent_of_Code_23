a70c71fd39509f373b28370e3231e8b8cd3fe03f
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-12-08 10:43:57 +0100

    Struggeling on Day 7 part 1

--- Day_7/src/part1.rs
+++ Day_7/src/part1.rs
@@ -1,8 +1,9 @@
 use std::fs::set_permissions;
 use nom::bytes::complete::tag;
-use nom::character::complete::newline;
+use nom::character::complete::{digit0, line_ending, newline};
+use nom::character::is_alphabetic;
 use nom::IResult;
-use nom::multi::separated_list1;
+use nom::multi::{separated_list0, separated_list1};
 use nom::sequence::preceded;
 
 
@@ -13,16 +14,36 @@ mod tests {
     fn test() {
 
         let input = "32T3K 765
-                        T55J5 684
-                        KK677 28
-                        KTJJT 220
-                        QQQJA 483";
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
 
         let final_n = process_input(input).to_string();
 
         assert_eq!("6440",final_n);
     }
 }
+
+enum Card {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Bub = 11,
+    Queen = 12,
+    King = 13,
+    Ass = 14,
+}
+enum HandTypes{
+    Five
+}
+
 /*
 Five of a kind
 Four of a kind
@@ -34,37 +55,33 @@ Highest card
 pub struct Hand<'a> {
     rank: Option<i32>,
     bit: &'a str,
-    hand: Vec<&'a str>,
+    hand: Vec<&'a char>,
     points: Option<i32>,
 }
 
 impl<'a> Hand<'a> {
 
 
-    fn set_hand_and_bit(&mut self,bit: &'a str, hand: Vec<&'a str>){
-        self.bit = bit;
-        self.hand = hand;
-    }
-
 
 }
 
 pub fn process_input(input: &str) -> i32{
 
-    let (input, hands) = separated_list1(newline, hand)(input).unwrap();
-
+    let (input, hands) = separated_list0(line_ending, hand_parser)(input).unwrap();
+    hands;
     0
 
 }
 
-pub fn hand(input: &str) -> IResult<&str, Hand> {
-    let bit = input.split(" ").collect::<Vec<&str>>()[1];
-    let (input, hand) = preceded(tag(""), tag(" "))(input)?;
+pub fn hand_parser(input: &str) -> IResult<&str, Hand> {
+    let input_split = input.split_whitespace().collect::<Vec<&str>>();
+    let bit = input_split[1];
+    let  hand = Vec::from_iter(input_split[0]);
 
     Ok((input, Hand {
         rank: None,
         bit,
-        hand    ,
+        hand,
         points: None,
     }))
 }
